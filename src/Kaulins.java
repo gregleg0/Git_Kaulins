bd5d38ce35aac8c6461de8a1e2ebce795880d8b1
Berzins <markussberzins040406@gmail.com>
2025-04-07 11:07:32 +0300

    While cikls

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -6,8 +6,10 @@ public class Kaulins {
 	public static void main(String[] args) {
 		int rezies;
 		Scanner scan = new Scanner(System.in);
+		do {
 		System.out.print("Cik reizes mest kauliņu: ");
 		rezies = scan.nextInt();
+		}while(rezies < 1);
 		scan.close();
 		mestKaulinu(rezies);
 	}
